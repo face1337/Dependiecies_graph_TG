b62202acb31741b468d092c3ed4f0b1d437e5a74
DragoDBZ <57758610+DragoDBZ@users.noreply.github.com>
2020-01-18 15:13:19 +0100

    Wczytanie pliku
    wczytanie pliku ze scieżki format (.py)

--- graph.py
+++ graph.py
@@ -10,3 +10,11 @@ import re
 
 current_file_name = sys.argv[0].split('/')[-1]  #return current file name
 
+# ######## Historyjka 1. #########
+def extract_filename(file): 
+    return file.split(".")[0]  
+
+
+def get_file_size(file_path): 
+    file_path = file_path if file_path.endswith('.py') else './'+file_path+'.py'
+    return os.path.getsize(file_path)
